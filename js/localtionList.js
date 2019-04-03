81127be764c32887e2924f73285dec5ccb27f57f
lucassingh <lucas.singh10@gmail.com>
2019-04-03 17:15:48 -0300

    Add files via upload

--- js/localtionList.js
+++ js/localtionList.js
@@ -3,22 +3,37 @@ $('#btn-locations').click(function(){
     $('#btn-reset-locations').show(500);
     $('#btn-locations').prop('disabled', true);
     $('#locations').show(500);
+    $('#map').show(500);
 });
 
 $('#btn-reset-locations').click(function(){
     $('#btn-reset-locations').hide(500);
     $('#btn-locations').prop('disabled', false);
     $('#locations').hide(500);
+    $('#map').hide(500);
 });
 
-var responseList = document.querySelector('#responseList');
+let responseList = document.querySelector('#responseList');
+let map;
 let positionList = [];
+let markers = [];
+
+window.addEventListener('load', 
+    function() { 
+        initMap();
+    }, false
+);
 
 function getLocationList() {
     
     fetch('http://api.open-notify.org/iss-now.json')
     .then(responseList => responseList.json())
     .then(data => {
+        // We update our cluster of Markers showing the last 15 positions of the ISS on the map
+        generateMapMarker(data.iss_position.latitude, data.iss_position.longitude);
+        // We center the map to the latest position we got from API
+        updateMapCenter(data.iss_position.latitude, data.iss_position.longitude);
+
         if (positionList.length < 15) {
             positionList.push({
                 latitude: data.iss_position.latitude, 
@@ -30,7 +45,6 @@ function getLocationList() {
                 latitude: data.iss_position.latitude, 
                 longitude: data.iss_position.longitude
             });
-            console.dir(positionList);
         }
 
         responseList.innerHTML = '';
@@ -42,10 +56,60 @@ function getLocationList() {
             </tr>
         `;
         });
+        
     })
     .then(() => {
-        setTimeout(getLocationList, 1000);
+        setTimeout(getLocationList, 2000);
     })
+}
+
+function initMap() {
+
+    fetch('http://api.open-notify.org/iss-now.json')
+    .then(responseList => responseList.json())
+    .then(data => {
+        //new map
+        map = new google.maps.Map(document.getElementById('map'), {
+
+        //map options
+        center: {lat: parseFloat(data.iss_position.latitude), lng: parseFloat(data.iss_position.longitude)},
+        zoom: 8
+        });
+    })
+}
+
+function updateMapCenter(latitude, longitude) {
+    let position = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
+    // debugger;
+    map.setCenter(position);
+}
+
+function generateMapMarker(latitude, longitude) {
+    let position = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
+
+      if (markers.length < 5) {
+        // debugger;
+        markers.push({
+            lat: position.lat, 
+            lng: position.lng
+        });
+      } else {
+        markers.splice(0, 1);
+        markers.push({
+            lat: position.lat, 
+            lng: position.lng
+        });
+    }
+    
+   
+    markers.forEach(currentMarker => {
+        new google.maps.Marker({
+            position: currentMarker,
+            title: 'new marker',
+            draggable: false,
+            map: map
+        }); 
+    });
 }
 
 function resetLocationList() {
